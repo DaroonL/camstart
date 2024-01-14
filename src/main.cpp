5c883e39f4c1ca7c292ac7738f2f37e751369fcf
Dean Mekulovic <deanmekulovic@gmail.com>
2024-01-13 22:06:09 -0500

    Start recording analysis

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include "cam_record.py"
+
 #include <opencv2/core.hpp>
+#include <opencv2/video/tracking.hpp>
+#include <opencv2/videoio.hpp>
+
+int main(int argc, char **argv) {
+    const char* path = "holder/hi";
+    const int fps = 15;
+    record(path,fps);
+    // create video file
+    VideoCapture recording = cv::VideoCapture::open(path);
+    cv::DISOpticalFlow optflow = cv::DISOpticalFlow::create(DISOpticalFlow::0)
+}
